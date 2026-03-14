84e7c0e20b39bcb7d90912cf4bea95b6d5199c41
Raffael Wagner <raffaelwagner@gmail.com>
2026-03-14 14:43:22 -0300

    add: adiciona interface da pilha e implemtação de pilha usando lista encadeada

--- src/main/tads/LinkedStack.java
+++ src/main/tads/LinkedStack.java
@@ -1,4 +1,72 @@
 package main.tads;
 
+/**
+ * Representa a implementação da estrutura: Pilha utilizando Lista Encadeada.
+ * <p>
+ * Esta classe implementa a interface {@link Stack} e atende ao requisito
+ * arquitetural de utilizar uma instância de Lista Encadeada para simular
+ * o comportamento de uma Pilha.
+ * </p>
+ *
+ * @param <E> o tipo de elemento que a pilha irá armazenar.
+ * @see Stack
+ * @see NodeList
+ * @author Raffael Wagner
+ * @version 1.0
+ * @since 2026-03-12
+ */
+
 public class LinkedStack<E> implements Stack<E> {
+
+    private NodeList<E> top;
+    private int size;
+
+    public LinkedStack() {
+        this.top = null;
+        this.size = 0;
+    }
+
+    @Override
+    public void push(E element) {
+        top = new NodeList<>(element, top);
+        size++;
+    }
+
+    @Override
+    public E pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Erro: A pilha está vazia.");
+        }
+
+        E data = top.getData();
+        top = top.getNext();
+        size--;
+
+        return data;
+    }
+
+    @Override
+    public E peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Erro: A pilha está vazia.");
+        }
+
+        return top.getData();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void clear() {
+        top = null;
+        size = 0;
+    }
 }
--- src/main/tads/Stack.java
+++ src/main/tads/Stack.java
@@ -1,4 +1,24 @@
 package main.tads;
 
+/**
+ * Representa a interface da estrutura de dados: Pilha (Stack).
+ * <p>
+ * Esta interface define as operações padronizadas para o comportamento
+ * de uma pilha baseada no princípio LIFO (Last-In-First-Out), garantindo
+ * que o último elemento inserido seja o primeiro a ser removido.
+ * </p>
+ *
+ * @param <E> o tipo de elemento que a pilha irá armazenar.
+ * @author Raffael Wagner
+ * @version 1.0
+ * @since 2026-03-11
+ */
 public interface Stack<E> {
+    void push(E element);
+    E pop();
+    E peek();
+    boolean isEmpty();
+    int size();
+    void clear();
+    
 }
